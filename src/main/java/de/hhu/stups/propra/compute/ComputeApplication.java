c5a1bee948ad9c1d5158c2a46be629fa6286259e
Jens Bendisposto <jens@bendisposto.de>
2017-11-30 17:05:37 +0100

    final version

---
+++ src/main/java/de/hhu/stups/propra/compute/ComputeApplication.java
@@ -0,0 +1,12 @@
+package de.hhu.stups.propra.compute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputeApplication.class, args);
+	}
+}
